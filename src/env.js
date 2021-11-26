4d69c40bbdf6c3c409aaf5bd5d6f95ca545d6225
Elie Andraos <elieandraos31@gmail.com>
2021-11-26 12:56:19 +0200

    :brick: env helpers

---
+++ src/env.js
@@ -0,0 +1,29 @@
+require('dotenv').config()
+import { abortWithMessage } from "./console";
+
+const hasEnv = (vars) => {
+    try {
+        let missing = []
+
+        vars.forEach((variable) => {
+            if (!process.env[variable]) missing.push(variable)
+        })
+
+        return !!missing.length
+    } catch (e) {
+        abortWithMessage(e)
+    }
+}
+
+const getEnv = (variable) => {
+    try {
+        return process.env[variable] || null
+    } catch (e) {
+        abortWithMessage(e)
+    }
+}
+
+export {
+    hasEnv,
+    getEnv
+}
